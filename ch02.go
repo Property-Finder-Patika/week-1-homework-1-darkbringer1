4a9701f4d6250465c5c22549772c63b5a2ea4c8f
Doğukaan Kılıçarslan <40922278+darkbringer1@users.noreply.github.com>
2022-06-25 23:23:59 +0300

    Add ch02 file
    Add:
    -Fahrenheit vs Celcius conversion methods

---
+++ ch02.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const boilingF = 212.0
+
+func main() {
+	boilingPoint()
+	ftocPrint()
+}
+
+func boilingPoint() {
+	var f = boilingF
+	var c = (f - 32) * 5 / 9
+	fmt.Printf("boiling point = %gF or %g C\n", f, c)
+}
+
+func ftocPrint() {
+	const freezingF, boilingF = 32.0, 212.0
+
+	fmt.Printf("%gF = %gC\n", freezingF, fToC(freezingF))
+	fmt.Printf("%gF = %gC\n", boilingF, fToC(boilingF))
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
+
